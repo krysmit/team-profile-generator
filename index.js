a37b7bf1dde0d0b4ad41c0de6fc4ed6b80b1dd7b
Krystyn Smith <krskrsmith@gmail.com>
2021-10-02 17:43:50 -0500

    added inquirer. linked required functionalities to my index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+var inquirer = require('inquirer');
+var fs = require('fs');
+const manager = require("./lib/manager");
+const engineer = require("./lib/engineer");
+const intern = require("./lib/intern");

9227d7de263fdeea4934eeb46d24b9024bd12c43
Krystyn Smith <krskrsmith@gmail.com>
2021-10-02 17:35:43 -0500

    i have no idea if it works becuase I'm getting an error, but the format of the test is correct. began writing more to define the employee class

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -3,9 +3,15 @@ const employee = require("../lib/employee");
 
 
 // give an input to the function
-
+describe("Name Input", () => {
+    test("name should be a string", () => {
+      const input = string;
 
 // define what to expect as the output
-
+const output = [{ id: 3, url: "https://www.link3.dev" }];
 
 // check if the function produces the expected output
+      expect(filterByTerm(input, string)).toEqual(output);
+  
+    });
+  });

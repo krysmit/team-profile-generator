239eb4d5107fd54f0773c0c3c85443e485a9f8aa
Krystyn Smith <krskrsmith@gmail.com>
2021-10-02 20:00:34 -0500

    fixed some syntax issues in the profiles.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var inquirer = require('inquirer');
 var fs = require('fs');
+const employee = require("./lib/employee");
 const manager = require("./lib/manager");
 const engineer = require("./lib/engineer");
 const intern = require("./lib/intern");

4f104ac403fa9f763840ba58878617ccb2a101c5
Krystyn Smith <krskrsmith@gmail.com>
2021-10-03 09:34:37 -0500

    email and github accounts are now clickable

--- dist/genHTML.js
+++ dist/genHTML.js
@@ -21,14 +21,17 @@ function generateMarkdown(data) {
 
         <div class="col-12 col-sm-6 col-md-3 mb-3">
           <div class="card">
-            <h4 id=role class="card-header">Employee Type</h4>
+            <h4 id=role class="card-header">${responses.role}</h4>
             <div class="card-body">
-              <p id=name class="card-text"></p>
-              <p id=id class="card-text"></p>
-              <p id=email class="card-text">asdf</p>
-              <p id=github class="card-text">sdfg</p>
-              <p id=office class="card-text">dfgh</p>
-              <p id=school class="card-text">fghj</p>
+              <p id=name class="card-text">Name: ${responses.name}</p>
+              <p id=id class="card-text">ID #: ${responses.id}</p>
+              <p id=email class="card-text">Email: 
+              <a href="mailto:${responses.id}">${responses.id}</a>
+              </p>
+              <p id=github class="card-text">Github: 
+              <a href="https://www.github.com/${responses.gihub}">${responses.github}</p>
+              <p id=office class="card-text">Office #: ${responses.office}</p>
+              <p id=school class="card-text">School: ${responses.school}</p>
             </div>
           </div>
         </div>

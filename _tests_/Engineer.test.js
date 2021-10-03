ad8658b0a8e78d54e50ea4573ccf29dc44aeb307
Krystyn Smith <krskrsmith@gmail.com>
2021-10-02 19:24:52 -0500

    I believe that I've formatted the tests correctly. don't quite know what to test yet, but as I complete the next few steps I think I'll have the hang of it. updated all js tests to be formatted in a similar way

--- _tests_/Engineer.test.js
+++ _tests_/Engineer.test.js
@@ -3,9 +3,15 @@ const engineer = require("../lib/engineer");
 
 
 // give an input to the function
-
+describe("School Input", () => {
+    test("name should be a string", () => {
+      const input = string;
 
 // define what to expect as the output
-
+const output = string;
 
 // check if the function produces the expected output
+      expect(filterByTerm(input, string)).toEqual(output);
+  
+    });
+  });
--- _tests_/Manager.test.js
+++ _tests_/Manager.test.js
@@ -3,9 +3,15 @@ const manager = require("../lib/manager");
 
 
 // give an input to the function
-
+describe("Office Number Input", () => {
+    test("name should be a string", () => {
+      const input = string;
 
 // define what to expect as the output
-
+const output = string;
 
 // check if the function produces the expected output
+      expect(filterByTerm(input, string)).toEqual(output);
+  
+    });
+  });
